c6dd90aa85a05412d769912b6fb2988b69b3a7ae
Eduardo Valerio Gamez <evaleriog@outlook.com>
2020-02-06 14:22:50 -0600

    Set up environment for project

---
+++ openhelp/src/main/java/com/capstone/openhelp/OpenhelpApplication.java
@@ -0,0 +1,13 @@
+package com.capstone.openhelp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenhelpApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OpenhelpApplication.class, args);
+    }
+
+}
---
+++ openhelp/src/test/java/com/capstone/openhelp/OpenhelpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.capstone.openhelp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpenhelpApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
