9356d961d0108d7a8141fefe1e19aca9ab707ce0
Bryan C Walsh <Walsh.C.Bryan@Gmail.com>
2020-02-07 16:59:35 -0600

    removed responsebody and fized spacing on comingsoon page

--- openhelp/src/main/java/com/capstone/openhelp/controllers/ComingSoonController.java
+++ openhelp/src/main/java/com/capstone/openhelp/controllers/ComingSoonController.java
@@ -5,11 +5,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
-@ResponseBody
 public class ComingSoonController {
-    @GetMapping("/comingsoon")
 
+    @GetMapping("/comingsoon")
     public String comingSoon(){
+
         return "comingsoon";
     }
 }
